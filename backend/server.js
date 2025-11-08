941c7e181a9440bd2e5b13cfb662fba0b139fefe
manikandan <manikandan110305@gmail.com>
2025-11-08 12:46:56 +0530

    update: improved Login UI/UX and removed bg image

--- backend/server.js
+++ backend/server.js
@@ -16,13 +16,16 @@ app.use(
   })
 );
 
-
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB Connected"))
   .catch((err) => console.error("❌ MongoDB Error:", err));
 
 app.use("/api/sessions", sessionRoutes);
+
+app.get("/", (req, res) => {
+  res.send("✅ API is running...");
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
